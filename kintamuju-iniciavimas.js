65f6a1f65c2a7036aad6f8736bccddd68ffb19e7
plukenis <ieva.zaboole@gmail.com>
2021-09-20 12:08:01 +0300

    let changed to const

--- kintamuju-iniciavimas.js
+++ kintamuju-iniciavimas.js
@@ -1,44 +1,44 @@
 console.log('----- 1 / a -----');
 
-let pirmas = 2;
+const pirmas = 2;
 console.log(pirmas);
 
-let antras = 5;
+const antras = 5;
 console.log(antras);
 
-let trecias = 7;
+const trecias = 7;
 console.log(trecias);
 
 console.log('----- 2 / a -----');
 
-let ketvirtas = 'Jonas';
+const ketvirtas = 'Jonas';
 console.log(ketvirtas);
 
-let penktas = 'Petras';
+const penktas = 'Petras';
 console.log(penktas);
 
-let sestas = 'Antanas';
+const sestas = 'Antanas';
 console.log(sestas);
 
 console.log('----- 3 / a -----');
 
-let septintas = [1, 2, 3, 4, 5];
+const septintas = [1, 2, 3, 4, 5];
 console.log(septintas);
 
-let astuntas = [12, 15, 35, 43, 56];
+const astuntas = [12, 15, 35, 43, 56];
 console.log(astuntas);
 
-let devintas = [223, 234, 465, 576, 788];
+const devintas = [223, 234, 465, 576, 788];
 console.log(devintas);
 
 console.log('----- 4 / a -----');
 
-let vienuoliktas = ['vienas', 'du', 'trys', 'keturi', 'penki']
+const vienuoliktas = ['vienas', 'du', 'trys', 'keturi', 'penki']
 console.log(vienuoliktas);
 
-let dvyliktas = ['Jonas', 'Petras', 'Antanas', 'Juozas', 'Vytautas'];
+const dvyliktas = ['Jonas', 'Petras', 'Antanas', 'Juozas', 'Vytautas'];
 console.log(dvyliktas);
 
-let tryliktas = ['Marija', 'Ona', 'Milda', 'Danute', 'Rita']
+const tryliktas = ['Marija', 'Ona', 'Milda', 'Danute', 'Rita']
 console.log(tryliktas);
 
