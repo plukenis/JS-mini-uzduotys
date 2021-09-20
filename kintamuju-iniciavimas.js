7b528328250d60c897ad23ecbb6d5e309226d14f
plukenis <ieva.zaboole@gmail.com>
2021-09-20 11:42:35 +0300

    kintamuju iniciavimas

---
+++ kintamuju-iniciavimas.js
@@ -0,0 +1,44 @@
+console.log('----- 1 / a -----');
+
+let pirmas = 2;
+console.log(pirmas);
+
+let antras = 5;
+console.log(antras);
+
+let trecias = 7;
+console.log(trecias);
+
+console.log('----- 2 / a -----');
+
+let ketvirtas = 'Jonas';
+console.log(ketvirtas);
+
+let penktas = 'Petras';
+console.log(penktas);
+
+let sestas = 'Antanas';
+console.log(sestas);
+
+console.log('----- 3 / a -----');
+
+let septintas = [1, 2, 3, 4, 5];
+console.log(septintas);
+
+let astuntas = [12, 15, 35, 43, 56];
+console.log(astuntas);
+
+let devintas = [223, 234, 465, 576, 788];
+console.log(devintas);
+
+console.log('----- 4 / a -----');
+
+let vienuoliktas = ['vienas', 'du', 'trys', 'keturi', 'penki']
+console.log(vienuoliktas);
+
+let dvyliktas = ['Jonas', 'Petras', 'Antanas', 'Juozas', 'Vytautas'];
+console.log(dvyliktas);
+
+let tryliktas = ['Marija', 'Ona', 'Milda', 'Danute', 'Rita']
+console.log(tryliktas);
+
