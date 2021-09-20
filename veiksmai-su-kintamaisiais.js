87e1ad24f1f77b8c16b199dfc0fa8f0e59d195fa
plukenis <ieva.zaboole@gmail.com>
2021-09-20 13:18:29 +0300

    veiksmai su kintamaisiais

---
+++ veiksmai-su-kintamaisiais.js
@@ -0,0 +1,30 @@
+console.log('------ 1 / a -------');
+
+const pirmas = 2;
+const antras = 6;
+const trecias = 8;
+
+const suma = pirmas + antras + trecias;
+console.log(suma);
+
+console.log('------ 2 / a --------');
+
+const ketvirtas = 'Labas';
+const penktas = 'Jonai';
+const sestas = 'Jonaiti';
+
+const pasisveikinimas = ketvirtas + ' ' + penktas + ' ' + sestas;
+console.log(pasisveikinimas);
+
+console.log('------ 3 / a.b --------');
+
+const skaiciai = [1, 2, 3, 4, 5]
+const apskaiciuoti = 1-2+3-4+5;
+console.log(apskaiciuoti);
+
+console.log('------ 4 --------');
+
+const septintas = ['Marija', 'Ona', 'Milda', 'Danute', 'Rita'];
+
+const vardai = septintas.join(', ');
+console.log(vardai);
